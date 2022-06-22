962be9df6140db4da3e53440a5898d3fb2527bc4
AsadsGit <asad22376@outlook.com>
2022-06-22 23:29:30 +0530

    Files added

---
+++ get_data.py
@@ -0,0 +1,28 @@
+__author__ = "Recep YILDIRIM"
+
+import pandas as pd
+import zipfile
+import urllib.request
+import os
+
+def get_data(folder_name="dataset", file_name="dataset.zip"):
+    url = "https://drive.google.com/file/d/1F-1jchSoW3KzZyC4v6zWT9FfC6FQM2UY/view?usp=sharing"
+    path = os.path.join(folder_name, file_name)
+
+    if not os.path.isdir(folder_name):
+        os.mkdir(folder_name)
+        print("'{}' folder was created.".format(folder_name))
+
+    else:
+        print("Dataset will downloaded to exist '{}' folder.".format(folder_name))
+
+    while os.path.isfile(path):
+        file_name = str(input("File exist, enter a new file name: ")) + ".zip"
+        path = os.path.join(folder_name, file_name)
+
+    urllib.request.urlretrieve(url, path)
+
+    with zipfile.ZipFile(path, "r") as dataset:
+        dataset.extractall(folder_name)
+
+    return pd.read_csv(folder_name + "/credit_train.csv"), pd.read_csv(folder_name + "/credit_test.csv")
